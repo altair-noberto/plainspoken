7171a743ee2227f255438f2ce1808b1a96b17b10
altair-noberto <altairnoberto@outlook.com.br>
2023-10-20 22:23:22 -0300

    added Scripts

---
+++ src/components/Scripts/DarkLight.js
@@ -0,0 +1,12 @@
+document.querySelector('[DarkLight]').addEventListener('click', DarkLight)
+  function DarkLight(){
+    const body = document.querySelector('body')
+    if(body.classList.contains('dark')) {
+      body.classList.remove('dark');
+      sessionStorage.setItem('DarkLight', 'light')
+    }
+    else {
+      body.classList.add('dark');
+      sessionStorage.setItem('DarkLight', 'dark')
+    }
+  }
---
+++ src/components/Scripts/Font.js
@@ -0,0 +1,12 @@
+document.querySelector('[Font]').addEventListener('click', Font)
+function Font(){
+  const body = document.querySelector('body')
+  if(sessionStorage.getItem('Font') === 'serif' || !sessionStorage.getItem('Font')){
+    sessionStorage.setItem('Font', 'sans-serif');
+    body.style.fontFamily = 'var(--font-body)';
+  }
+  else {
+    sessionStorage.setItem('Font', 'serif');
+    body.style.fontFamily = 'initial';
+  }
+}
