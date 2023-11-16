704da0ef227f5553001e02d978c5a09b25c504d5
altair-noberto <altairnoberto@outlook.com.br>
2023-11-16 18:42:32 -0300

    font tweaks

--- src/components/Scripts/Font.js
+++ src/components/Scripts/Font.js
@@ -4,12 +4,12 @@ document.addEventListener('astro:page-load', () => {
 
 function Font(){
   const body = document.querySelector('body')
-  if(localStorage.getItem('Font') === 'serif' || !localStorage.getItem('Font')){
-    localStorage.setItem('Font', 'sans-serif');
-    body.style.fontFamily = 'var(--font-body)';
-  }
-  else {
+  if(localStorage.getItem('Font') === 'sans-serif' || !localStorage.getItem('Font')){
     localStorage.setItem('Font', 'serif');
     body.style.fontFamily = '\'Libre Bodoni\', serif';
+  }
+  else {
+    localStorage.setItem('Font', 'sans-serif');
+    body.style.fontFamily = 'var(--font-body)';
   }
   }
